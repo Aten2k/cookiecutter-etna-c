6e4286c38b5cd9a1dfbcccc37082990c6dd8eed9
Theo Massard <massar_t@etna-alternance.net>
2018-01-07 01:54:53 +0100

    Remove clutter

--- {{cookiecutter.project_name|lower}}/src/{{cookiecutter.project_name|lower}}.c
+++ {{cookiecutter.project_name|lower}}/src/{{cookiecutter.project_name|lower}}.c
@@ -19,69 +19,8 @@
 
 #include "{{cookiecutter.project_name|lower}}.h"
 
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <getopt.h>
-
-extern const char *__progname;
-
-static void
-usage(void)
+int main(int argc, char **argv)
 {
-	/* TODO:3002 Don't forget to update the usage block with the most
-	 * TODO:3002 important options. */
-	fprintf(stderr, "Usage: %s [OPTIONS]\n",
-	    __progname);
-	fprintf(stderr, "Version: %s\n", PACKAGE_STRING);
-	fprintf(stderr, "\n");
-	fprintf(stderr, " -d, --debug        be more verbose.\n");
-	fprintf(stderr, " -h, --help         display help and exit\n");
-	fprintf(stderr, " -v, --version      print version and exit\n");
-	fprintf(stderr, "\n");
-	fprintf(stderr, "see manual page " PACKAGE "(8) for more information\n");
-}
-
-int
-main(int argc, char *argv[])
-{
-	int debug = 1;
-	int ch;
-
-	/* TODO:3001 If you want to add more options, add them here. */
-	static struct option long_options[] = {
-                { "debug", no_argument, 0, 'd' },
-                { "help",  no_argument, 0, 'h' },
-                { "version", no_argument, 0, 'v' },
-		{ 0 }
-	};
-	while (1) {
-		int option_index = 0;
-		ch = getopt_long(argc, argv, "hvdD:",
-		    long_options, &option_index);
-		if (ch == -1) break;
-		switch (ch) {
-		case 'h':
-			usage();
-			exit(0);
-			break;
-		case 'v':
-			fprintf(stdout, "%s\n", PACKAGE_VERSION);
-			exit(0);
-			break;
-		case 'd':
-			debug++;
-			break;
-		case 'D':
-			log_accept(optarg);
-			break;
-		default:
-			fprintf(stderr, "unknown option `%c'\n", ch);
-			usage();
-			exit(1);
-		}
-	}
-
 	log_init(debug, __progname);
 
 	/* TODO:3000 It's time for you program to do something. Add anything
